ef9f790fa787ece54b9e38bf71a207253b8c6c1b
Nikithavedanth <nikithavedanth@gmail.com>
2024-03-21 13:33:43 -0400

    Updated

--- bgChanger/App.js
+++ bgChanger/App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [randomBackground, setRandomBackground] =useState('#ffffff');
+  const generateColor =()=>{
+    const hexRange='0123456789ABCDEF'
+    let color = "#"
+    for (let i=0;i<6;i++){
+      color += hexRange[Math.floor(Math.random()*16)]
+    }
+    setRandomBackground(color);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar backgroundColor={randomBackground}/>
+       <View style={[styles.container, {backgroundColor:randomBackground}]}>
+        <TouchableOpacity onPress={generateColor}>
+          <View styles={styles.actionBtn}>
+            <Text style={styles.actionBtnTxt}>Press me</Text>
+          </View>
+        </TouchableOpacity>
+       </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  actionBtn:{
+    borderRadius:12,
+    border: 2,
+    backgroundColor:'#6A1B4D',
+    paddingVertical:10,
+    paddingHorizontal:40,
+  },
+  actionBtnTxt:{
+    fontSize:24,
+    fontWeight:'bold',
+    textTransform:'uppercase',
+    color:'white',
+    // backgroundColor:'#6A1B4D',
+    backgroundColor:'#6A1B4D',
+    paddingVertical:10,
+    paddingHorizontal:40,
+    borderRadius:12,
   },
 });
